557e66d079b1c731da3ede47df0051c2fa0b8508
unknown <oseeronlan@gmail.com>
2021-11-16 14:08:56 +0100

    [chore]:add files and update readme documentation

---
+++ components/About.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="about py-5" id="about">
+    <div class="container py-5 my-5">
+      <h2 class="text-center mb-5">ABOUT</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Eget nulla facilisi
+        etiam dignissim diam quis. Tellus rutruLorem ipsum dolor sit amet,
+        consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+        et dolore magna aliqua. Ut enim ad minim veniam,
+      </p>
+      <p>
+        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+        consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+        esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim
+        id est laborum.
+      </p>
+      <div class="d-flex justify-content-center w-100">
+        <button class="btn btn-full mt-5 font-bold text-center">
+          Visit Not A Barista
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
+.about {
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),
+    url('~/assets/images/istockphoto-1066887298-612x612.jpg');
+  color: #e5e5e5;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  p {
+    line-height: 221%;
+  }
+}
+</style>
---
+++ components/Converter.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="converter">
+    <div class="container pt-5">
+      <!-- <h1>Converter</h1> -->
+      <div class="row">
+        <div class="col-lg-6">
+          <b-form class="my-5">
+            <b-form-group
+              class="mb-4"
+              id="input-group-1"
+              label="Grinder"
+              label-for="input-1"
+            >
+              <v-select
+                id="input-1"
+                :options="['Commandante C40']"
+              /> </b-form-group
+            ><b-form-group
+              class="mb-4"
+              id="input-group-2"
+              label="Brew method"
+              label-for="input-2"
+            >
+              <v-select id="input-2" :options="['Filter']" /> </b-form-group
+            ><b-form-group
+              class="mb-4"
+              id="input-group-3"
+              label="Grind size"
+              label-for="input-3"
+            >
+              <v-select id="input-3" :options="['Small']" />
+            </b-form-group>
+
+            <b-form-checkbox class="mb-2 mr-sm-2 mb-sm-0"
+              >Advanced Options</b-form-checkbox
+            >
+          </b-form>
+        </div>
+        <div class="col-lg-6">
+          <div
+            class="
+              h-100
+              w-100
+              p-3
+              d-flex
+              flex-xl-row flex-column
+              justify-content-lg-end
+              align-items-center
+            "
+          >
+            <img src="~/assets/images/grinder.png" />
+            <div class="text-center px-3">
+              <p class="mb-0">Filter medium fine between</p>
+              <h3>25-35 clicks</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.converter {
+  min-height: calc(100vh - 60px);
+  background: url('~/assets/images/coffeegrains.png');
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center;
+
+  label {
+    margin-bottom: 1rem;
+    font-weight: 700;
+  }
+
+  h3 {
+    font-size: 2.5rem;
+    font-weight: 700;
+  }
+}
+</style>
