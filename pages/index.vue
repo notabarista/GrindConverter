a1e9f04ff8776eb01754ce10ca5f4779a2d9ee08
unknown <oseeronlan@gmail.com>
2021-11-16 21:46:42 +0100

    [chore]: add ui integration

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="landing">
+    <Navbar />
+    <Converter />
+    <About />
+    <Faq />
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+<style lang="scss" scoped>
+.landing {
+  color: #3f3f46;
+  min-height: 100vh;
+}
+</style>
